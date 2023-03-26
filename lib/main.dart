99d4b7f533d0fa267b8f989246727b80810c3dc4
NaiYu <boo20750242@gmail.com>
2023-03-27 01:49:11 +0800

    Add: ItemInfo and ItemsGenerator

--- lib/main.dart
+++ lib/main.dart
@@ -79,6 +79,29 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class ItemsGenerator {
+  List<ItemInfo> list1 = List<ItemInfo>.generate(
+      20,
+      (index) =>
+          ItemInfo('UNIQLO Coat', 323, 'assets/JupiterVenus_Luy_960.jpg'));
+  List<ItemInfo> list2 = List<ItemInfo>.generate(
+      20,
+      (index) => ItemInfo('Adidas Coat', 250,
+          'assets/VenusJupiterSky_Tumino_1080_annotated.jpg'));
+  List<ItemInfo> list3 = List<ItemInfo>.generate(
+      20,
+      (index) => ItemInfo(
+          'Nike Coat', 800, 'assets/10_Days_of_Venus_and_Jupiter.jpeg'));
+}
+
+class ItemInfo {
+  final String title;
+  final int price;
+  final String imagePath;
+
+  ItemInfo(this.title, this.price, this.imagePath);
+}
+
 class ItemList extends StatelessWidget {
   const ItemList({
     super.key,
