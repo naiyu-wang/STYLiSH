d609d280254ca049f59c107bfb8e5911c757a22a
NaiYu <boo20750242@gmail.com>
2023-04-05 15:59:57 +0800

    Add: item brief info in ItemDetailPage

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Program App',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
       ),
       home: const MyHomePage(),
     );
@@ -36,6 +37,7 @@ class MyHomePage extends StatelessWidget {
     var items = ItemsGenerator();
 
     return Scaffold(
+      backgroundColor: theme.colorScheme.primaryContainer,
       appBar: AppBar(
         title: Image.asset('assets/stylish_logo.png',
             width: 150, fit: BoxFit.cover),
@@ -146,6 +148,89 @@ class ItemDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(itemInfo.title);
+    var theme = Theme.of(context);
+    var titleStyle = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.primary,
+    );
+    var infoStyle = theme.textTheme.bodyMedium!.copyWith(
+      color: theme.colorScheme.primary,
+    );
+
+    return Scaffold(
+        backgroundColor: theme.colorScheme.primaryContainer,
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(width: 500, child: Image.asset(itemInfo.imagePath)),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  width: 300,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(itemInfo.title, style: titleStyle),
+                      Text(DateTime.now().toString(), style: infoStyle),
+                      Text(
+                        'NT\$${itemInfo.price}',
+                        style: titleStyle,
+                      ),
+                      Container(
+                          width: double.infinity,
+                          height: 1.0,
+                          color: theme.colorScheme.primary),
+                      OptionBox(
+                          title: 'color',
+                          options: 'options',
+                          infoStyle: infoStyle,
+                          theme: theme),
+                      OptionBox(
+                          title: 'size',
+                          options: 'options',
+                          infoStyle: infoStyle,
+                          theme: theme),
+                      OptionBox(
+                          title: 'amount',
+                          options: 'options',
+                          infoStyle: infoStyle,
+                          theme: theme)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
+
+class OptionBox extends StatelessWidget {
+  const OptionBox({
+    super.key,
+    required this.title,
+    required this.options,
+    required this.infoStyle,
+    required this.theme,
+  });
+
+  final String title;
+  final String options;
+  final TextStyle infoStyle;
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 50.0,
+        child: Row(children: [
+          Text(title, style: infoStyle),
+          Container(
+              width: 1.0,
+              height: 30.0,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              color: theme.colorScheme.primary),
+          Text(options)
+        ]));
   }
 }
